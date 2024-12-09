fb57e540e0ebb9560cdbbbbd510c72f7d5f03c4f
Max Fuxjäger <maxvalue.git@gmail.com>
2024-12-09 01:33:26 +0100

    Format python code
    * spacing and newlines normalized
    * unused imports removed
    * global namespace imports replaced with normal imports
        * as aconsequence the usage of config values changed

--- quiz/app.py
+++ quiz/app.py
@@ -1,149 +1,155 @@
-
 import csv
 import random
 from flask import Flask
 from flask import render_template
-from flask import request
 from flask import session
-from flask import abort, redirect, url_for
-from vogel_constants import *
+import vogel_constants as config
 
 from hellodrinkbot import HelloDrinkbot
+
 # If we don't have a Motor hat set EMULATION. We can still run the quiz,
 # we just won't get a cocktail
-birds=[]
+birds = []
 try:
-    hd= HelloDrinkbot()
-    EMULATION=0
-except:
-    EMULATION=1
+    hd = HelloDrinkbot()
+    EMULATION = 0
+except Exception:
+    EMULATION = 1
 
 app = Flask(__name__)
-app.secret_key = b'foobar'
-app.debug=True
+app.secret_key = b"foobar"
+app.debug = True
+
 
 def read_birds():
-    """ reads the list of possible bird answers """
-    with open('birds_all.csv',newline='', encoding="UTF8") as lst:
+    """reads the list of possible bird answers"""
+    with open("birds_all.csv", newline="", encoding="UTF8") as lst:
         return [b.strip() for b in lst.readlines()]
 
+
 def read_quiz():
-    """ Read the list of possible questions, with the names and pictures """
-    quizlst=[]
-    with open('birds_quiz.csv',newline='', encoding="UTF8") as quizfile:
-        reader=csv.reader(quizfile)
+    """Read the list of possible questions, with the names and pictures"""
+    quizlst = []
+    with open("birds_quiz.csv", newline="", encoding="UTF8") as quizfile:
+        reader = csv.reader(quizfile)
         for row in reader:
-            quizlst.append({'num':row[0],'name':row[1],'url':row[2],'img':row[3]})
+            quizlst.append({"num": row[0], "name": row[1], "url": row[2], "img": row[3]})
     return quizlst
 
+
 def pick_question_bird():
-    """ Which bird are we going to show next? """
+    """Which bird are we going to show next?"""
     while True:
-        bird_num = random.randint(0, len(quizlst)-1)
-        if bird_num not in session['used']:
-            session['used'].append(bird_num)
+        bird_num = random.randint(0, len(quizlst) - 1)
+        if bird_num not in session["used"]:
+            session["used"].append(bird_num)
             return quizlst[bird_num]
+
 
 @app.route("/quiz_results")
 def quiz_results():
-    """ return the results """
-    p={}
-    p['results']=session['results']
-    session['results']=[]
-    p['page_title']='Vogel-Quiz Antworten!'
+    """return the results"""
+    p = {}
+    p["results"] = session["results"]
+    session["results"] = []
+    p["page_title"] = "Vogel-Quiz Antworten!"
     return render_template("quiz_results.html", p=p)
 
-def check_answer(answer):
-    """ check their answer and dispense (or not) the cocktail """
 
-    if 'bird_name' in session:
-        if session['bird_name']==answer:
+def check_answer(answer):
+    """check their answer and dispense (or not) the cocktail"""
+
+    if "bird_name" in session:
+        if session["bird_name"] == answer:
             if EMULATION:
                 # don't dispense, we are in emulation mode
                 pass
             else:
-                hd.dispense(AVENO_PUMP, AVENO_TIME) # Dispense AVENO
-                hd.dispense(LEMON_PUMP, LEMON_TIME) # Dispense LEMON
+                hd.dispense(config.AVENO_PUMP, config.AVENO_TIME)  # Dispense AVENO
+                hd.dispense(config.LEMON_PUMP, config.LEMON_TIME)  # Dispense LEMON
         else:
             # sadly they were wrong.
             pass
 
         # keep track of the results of each question, to display at the end
         try:
-            rslt={'bird_name':session['bird_name'], 'img':session['img'], 'answer':answer}
-        except:
-            rslt={'bird_name':'broken', 'img':'broken', 'answer':'broken'}
-        session['results'].append(rslt)
+            rslt = {"bird_name": session["bird_name"], "img": session["img"], "answer": answer}
+        except Exception:
+            rslt = {"bird_name": "broken", "img": "broken", "answer": "broken"}
+        session["results"].append(rslt)
     else:
         # this is the first question, and there should not be results yet.
-        session['results']=[]
+        session["results"] = []
+
 
 @app.route("/quiz/<answer>")
 @app.route("/quiz")
 def quiz(answer=None):
-    """ show the quiz, and results, and dispense the cocktail. This code
-        suffers from a lack of elegance, but it partially makes up for that
-        by appearing to work.
-        It uses 'bird' as both a name, and a dict. That is lame"""
-    p={}
+    """show the quiz, and results, and dispense the cocktail. This code
+    suffers from a lack of elegance, but it partially makes up for that
+    by appearing to work.
+    It uses 'bird' as both a name, and a dict. That is lame"""
+    p = {}
 
-    if 'question_number' in session:
-        session['question_number'] += 1
+    if "question_number" in session:
+        session["question_number"] += 1
     else:
-        session['question_number']=1
-        session['correct_count]'] = 0
-        session['results]'] = {}
+        session["question_number"] = 1
+        session["correct_count]"] = 0
+        session["results]"] = {}
 
-    if 'used' not in session:
-        session['used']=[]
+    if "used" not in session:
+        session["used"] = []
 
-    check_answer(answer) # checks answer, dispenses drink
+    check_answer(answer)  # checks answer, dispenses drink
     # Is their round over? clear the session and return results.
-    if session['question_number'] > NUM_ROUNDS:
-        p['results']=session['results']
-        p['page_title']='Vogel-Quiz Antworten!'
+    if session["question_number"] > config.NUM_ROUNDS:
+        p["results"] = session["results"]
+        p["page_title"] = "Vogel-Quiz Antworten!"
         session.clear()
         return render_template("quiz_results.html", p=p)
 
-    p['birds']=birds
-    p['quizlst']=quizlst
-    session['bird']=pick_question_bird()
-    session['bird_name']=session['bird']['name']
-    session['img']=session['bird']['img']
-    p['qcnt']=1
-    p['q']=session['bird']['num']
-    p['answers']=[]
-    p['answers'].append(session['bird']['name'])
-    while len(p['answers'])<NUM_ANSWERS:
-        a_num = random.randint(0, len(birds)-1)
+    p["birds"] = birds
+    p["quizlst"] = quizlst
+    session["bird"] = pick_question_bird()
+    session["bird_name"] = session["bird"]["name"]
+    session["img"] = session["bird"]["img"]
+    p["qcnt"] = 1
+    p["q"] = session["bird"]["num"]
+    p["answers"] = []
+    p["answers"].append(session["bird"]["name"])
+    while len(p["answers"]) < config.NUM_ANSWERS:
+        a_num = random.randint(0, len(birds) - 1)
         answer_name = birds[a_num]
-        if answer_name not in p['answers'] and answer_name != session['bird']['name']:
-            p['answers'].append(p['birds'][a_num])
-    random.shuffle(p['answers'])
+        if answer_name not in p["answers"] and answer_name != session["bird"]["name"]:
+            p["answers"].append(p["birds"][a_num])
+    random.shuffle(p["answers"])
 
-    p['page_title']='Vogel-Quiz!'
+    p["page_title"] = "Vogel-Quiz!"
     return render_template("quiz.html", p=p)
+
 
 @app.route("/")
 def home():
-    p={}
-    p['page_title']='Home page!'
+    p = {}
+    p["page_title"] = "Home page!"
     return render_template("home.html", p=p)
     # path to quiz
     # path to admin
 
+
 @app.route("/admin")
 def admin():
-    p={}
-    p['birds']=birds
-    p['birds'].sort()
-    p['quizlst']=quizlst
-    with open('vogel_constants.py') as vc:
-    	lst=vc.readlines()
+    p = {}
+    p["birds"] = birds
+    p["birds"].sort()
+    p["quizlst"] = quizlst
+    with open("vogel_constants.py") as vc:
+        lst = vc.readlines()
 
-    p['vogel_constants'] = ''.join(lst)
+    p["vogel_constants"] = "".join(lst)
 
-    p['page_title']='List of possible birds'
+    p["page_title"] = "List of possible birds"
     return render_template("admin.html", p=p)
 
 # quiz: welcome
@@ -161,6 +167,5 @@ def admin():
 # quiz: results
 
 
-
-birds=read_birds()
-quizlst=read_quiz()
+birds = read_birds()
+quizlst = read_quiz()
--- quiz/vogel_constants.py
+++ quiz/vogel_constants.py
@@ -1,15 +1,15 @@
 # Constants, feel free to adjust these
-NUM_ANSWERS=3 # how many birds are shown as possible answers
-NUM_ROUNDS=3  # how many birds you are shown
+NUM_ANSWERS = 3  # how many birds are shown as possible answers
+NUM_ROUNDS = 3  # how many birds you are shown
 
 # Calibration - I ran each pump several times and measured
 # the amount pumped and calculated the number of seconds to dispense 1 ml
 # I don't think these are totally stable. Adjust as needed.
-AVENO_PUMP = 0 # Pump number for the Aveno
-LEMON_PUMP = 2 # Pump number for the Lemon
-AVENO_SIZE = 4 # ml
-AVENO_SEC_ML = .81
+AVENO_PUMP = 0  # Pump number for the Aveno
+LEMON_PUMP = 2  # Pump number for the Lemon
+AVENO_SIZE = 4  # ml
+AVENO_SEC_ML = 0.81
 AVENO_TIME = round(AVENO_SIZE * AVENO_SEC_ML)
-LEMON_SIZE = 1.2 # ml
-LEMON_SEC_ML = .91
+LEMON_SIZE = 1.2  # ml
+LEMON_SEC_ML = 0.91
 LEMON_TIME = round(LEMON_SIZE * LEMON_SEC_ML)
